d1c90fd800c788c9cc7b9c788e84a5cb8d564eb5
BenedictRM <mehringr@gmail.com>
2014-12-23 23:31:20 -0700

    Update
    update

--- Game.java
+++ Game.java
@@ -33,7 +33,7 @@ public class Game {
 		
 		try{
 			//zeroDateTimeBehavior=convertToNull converts 0:00:00etc. timestamps to null preventing the system from crashing
-			connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/game?zeroDateTimeBehavior=convertToNull", "your username (try root)","Your MySQL Password");
+			connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/game?zeroDateTimeBehavior=convertToNull", "your username","your password");
 		}catch(SQLException o){
 			o.printStackTrace();
 		}
